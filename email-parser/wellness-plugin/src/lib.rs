bfb1b2513d0bda5fa9808ee2e319d335e59b8bec
Alexander Daily <alexanderdaily001@gmail.com>
2023-05-05 22:16:40 +0200

    can run plugin

--- email-parser/wellness-plugin/src/lib.rs
+++ email-parser/wellness-plugin/src/lib.rs
@@ -1,21 +1,16 @@
-use rhai::plugin::*;
+pub mod api;
 
-#[export_module]
-mod wellness_plugin {
-    // Build your module here.
-
-    pub fn test() {
-        print!("hello world");
-    }
-}
+use rhai_dylib::rhai::{
+   config::hashing::set_ahash_seed, exported_module, ImmutableString, Module, Shared
+};
 
 /// Export the wellness_plugin module.
+#[allow(improper_ctypes_definitions)]
 #[no_mangle]
-pub extern "C" fn module_entrypoint() -> rhai::Shared<rhai::Module> {
+pub extern "C" fn module_entrypoint() -> Shared<Module> {
+    set_ahash_seed(Some([1, 2, 3, 4])).unwrap();
 
-    // The seed must be the same as the one used in the program that will
-    // load this module.
-    rhai::config::hashing::set_ahash_seed(Some([1, 2, 3, 4])).unwrap();
+    println!("plugin: {:?}", std::any::TypeId::of::<ImmutableString>());
 
-    rhai::exported_module!(wellness_plugin).into()
+    exported_module!(api::wellness_plugin).into()
 }
