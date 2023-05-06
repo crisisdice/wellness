1e99872c85ea36d9b98f905ae78f4de0eaa7cb4c
root <root@server--1>
2023-05-06 12:16:33 -0400

    update path

--- req.sh
+++ req.sh
@@ -3,25 +3,24 @@ CUTOFF="2023-04-22" # inclusive
 
 PW_HASH=$(cat .password)
 DEBUG="--offline"
+SEARCH="grep" #macos ggrep
 
 URL='https://amdocs.globalfitnesschallenge.com/v1'
 POST_ENDPOINT='/users/110867/events'
 AUTH_ENDPOINT='/auth/login/me?utcOffset=120'
 AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64 AppleWebKit/537.36 )KHTML, like Gecko)( Chrome/105.0.0.0 Safari/537.36)'
 
-if [ $DEBUG = '' ]; then
-  HEADERS=$(http GET $URL$AUTH_ENDPOINT "Authorization: Basic $PW_HASH" -v)
+HEADERS=$(http GET $URL$AUTH_ENDPOINT "Authorization: Basic $PW_HASH" -v)
 
-  TOKEN=$(echo $HEADERS | ggrep -oP '(?<=Set-Cookie: jwtToken=)[^;]*')
-  XSRF=$(echo $HEADERS | ggrep -oP '(?<=Set-Cookie: XSRF-TOKEN=)[^;]*')
-  COOKIE="jwtToken=$TOKEN"
-fi
+TOKEN=$(echo $HEADERS | $SEARCH -oP '(?<=Set-Cookie: jwtToken=)[^;]*')
+XSRF=$(echo $HEADERS | $SEARCH -oP '(?<=Set-Cookie: XSRF-TOKEN=)[^;]*')
+COOKIE="jwtToken=$TOKEN"
 
 IFS=$'\n'
-declare -a ACTIONS=($(./a.out $CUTOFF))
+declare -a ACTIONS=($(wellness-xml-parser $CUTOFF))
 
 for BODY in "${ACTIONS[@]}"; do
-  echo $BODY | http DEBUG POST $URL$POST_ENDPOINT "Authorization: Bearer $TOKEN" \
+  echo $BODY | http $DEBUG POST $URL$POST_ENDPOINT "Authorization: Bearer $TOKEN" \
     "Cookie: $COOKIE" "Origin: $URL" "User-Agent: $AGENT" "X-XSRF-Token: $XSRF" -v
 done
 
